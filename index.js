58add44f558f7cc918b2474cac367f0d9a7cd29f
Ivan Garcia <ivanggv2@gmail.com>
2023-01-23 22:37:23 +0100

    express

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const port = process.env.PORT || 4000;
+const express = require("express");
 
 jsonServer.defaults({ readOnly: false });
 server.use(express.json());
